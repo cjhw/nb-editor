074c2f166ecd8c62f1efd7f991af5cdf77092e60
cjh <1784581164@qq.com>
2024-08-06 23:48:06 +0800

    feat: add eslint rule and add swc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,5 +2,15 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   react: true,
-  rules: { 'node/prefer-global/process': 'off', 'no-console': 'off', 'ts/consistent-type-imports': 'off' },
+  rules: {
+    'node/prefer-global/process': 'off',
+    'no-consdfdfole': 'off',
+    'ts/consistent-type-imports': 'off',
+    'style/comma-dangle': 'off',
+    'style/brace-style': 'off',
+    'antfu/if-newline': 'off',
+    'style/jsx-one-expression-per-line': 'off',
+    'no-console': 'off',
+  },
+  ignores: ['**/dist/**', '**/node_modules/**'],
 })
